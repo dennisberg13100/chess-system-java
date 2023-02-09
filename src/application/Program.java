6fec12f3d9bb222bd81d1d8c37717dd4780c18b4
Dennis van den Berg <dennisvandenberg13100@gmail.com>
2023-02-09 09:54:44 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,16 +18,18 @@ public class Program {
 		while(true) {
 			try {
 				UI.clearScreen();
-				UI.pritnBoard(chessMatch.getPieces());
+				UI.printBoard(chessMatch.getPieces());
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
-				System.out.println();
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				
+				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
